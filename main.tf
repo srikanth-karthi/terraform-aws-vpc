f2dd6b53adbd7b988a856c8110f91c9af4ac4971
SRIKANTH K <97454625+srikanth-karthi@users.noreply.github.com>
2025-03-20 13:57:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "srikanth_vpc" {
+resource "aws_vpc" "main_vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   tags = {
@@ -6,29 +6,29 @@ resource "aws_vpc" "srikanth_vpc" {
   }
 }
 
-resource "aws_internet_gateway" "srikanth_igw" {
-  vpc_id = aws_vpc.srikanth_vpc.id
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
   tags = {
     Name = "${var.vpc_name}-igw"
   }
 }
 
-resource "aws_route_table" "srikanth_route_table" {
-  vpc_id = aws_vpc.srikanth_vpc.id
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
   tags = {
     Name = "${var.vpc_name}-route-table"
   }
 }
 
 resource "aws_route" "internet_route" {
-  route_table_id         = aws_route_table.srikanth_route_table.id
+  route_table_id         = aws_route_table.main_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.srikanth_igw.id
+  gateway_id             = aws_internet_gateway.main_igw.id
 }
 
 resource "aws_subnet" "public" {
   count             = length(var.public_subnets)
-  vpc_id            = aws_vpc.srikanth_vpc.id
+  vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = element(var.public_subnets, count.index)
   availability_zone = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true 
@@ -41,11 +41,11 @@ resource "aws_subnet" "public" {
 resource "aws_route_table_association" "public_subnet_association" {
   count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.srikanth_route_table.id
+  route_table_id = aws_route_table.main_route_table.id
 }
 
-resource "aws_route_table" "srikanth_private_route_table" {
-  vpc_id = aws_vpc.srikanth_vpc.id
+resource "aws_route_table" "main_private_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
   tags = {
     Name = "${var.vpc_name}-private-route-table"
   }
@@ -53,7 +53,7 @@ resource "aws_route_table" "srikanth_private_route_table" {
 
 resource "aws_subnet" "private" {
   count             = length(var.private_subnets)
-  vpc_id            = aws_vpc.srikanth_vpc.id
+  vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = element(var.private_subnets, count.index)
   availability_zone = element(var.availability_zones, count.index)
 
@@ -65,8 +65,5 @@ resource "aws_subnet" "private" {
 resource "aws_route_table_association" "private_subnet_association" {
   count          = length(aws_subnet.private)
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.srikanth_private_route_table.id
+  route_table_id = aws_route_table.main_private_route_table.id
 }
-
-
-
