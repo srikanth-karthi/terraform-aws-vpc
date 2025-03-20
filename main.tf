8c3e9659765212c2fec59f5130909e42e242333b
srikanth-karthi <srikanth.k.c@greenwayhealth.com>
2025-03-20 13:59:56 +0530

    Remove unused output variables from main.tf

--- main.tf
+++ main.tf
@@ -68,26 +68,4 @@ resource "aws_route_table_association" "private_subnet_association" {
   route_table_id = aws_route_table.main_private_route_table.id
 }
 
-output "vpc_id" {
-  value = aws_vpc.main_vpc.id
-}
 
-output "public_subnet_1_id" {
-  value = aws_subnet.public[0].id
-}
-
-output "public_subnet_2_id" {
-  value = aws_subnet.public[1].id
-}
-
-output "private_subnet_1_id" {
-  value = aws_subnet.private[0].id
-}
-
-output "private_subnet_2_id" {
-  value = aws_subnet.private[1].id
-}
-
-output "vpc_cidr_block" {
-  value = aws_vpc.main_vpc.cidr_block
-}
