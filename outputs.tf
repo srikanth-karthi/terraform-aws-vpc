00098075f2bff0fa5cbdf744e4a4a73ede88f7c9
srikanth-karthi <srikanthkarthi2003@gmail.com>
2025-04-26 08:49:00 +0530

    docs: add descriptions to output blocks

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,29 @@
 output "vpc_id" {
-  value = aws_vpc.main_vpc.id
+  description = "The ID of the main VPC"
+  value       = aws_vpc.main_vpc.id
 }
 
 output "public_subnet_1_id" {
-  value = aws_subnet.public[0].id
+  description = "The ID of the first public subnet in the VPC"
+  value       = aws_subnet.public[0].id
 }
 
 output "public_subnet_2_id" {
-  value = aws_subnet.public[1].id
+  description = "The ID of the second public subnet in the VPC"
+  value       = aws_subnet.public[1].id
 }
 
 output "private_subnet_1_id" {
-  value = aws_subnet.private[0].id
+  description = "The ID of the first private subnet in the VPC"
+  value       = aws_subnet.private[0].id
 }
 
 output "private_subnet_2_id" {
-  value = aws_subnet.private[1].id
+  description = "The ID of the second private subnet in the VPC"
+  value       = aws_subnet.private[1].id
 }
 
 output "vpc_cidr_block" {
-  value = aws_vpc.main_vpc.cidr_block
+  description = "The CIDR block of the main VPC"
+  value       = aws_vpc.main_vpc.cidr_block
 }
